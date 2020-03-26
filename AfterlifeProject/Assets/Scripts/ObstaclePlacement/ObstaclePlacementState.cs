6a1f9eb3e0c69bd27b3b36e9cbf59b1057d17347
Loims <34040415+Loims@users.noreply.github.com>
2020-03-26 14:56:57 -0400

    Add basic level progression

---
+++ AfterlifeProject/Assets/Scripts/ObstaclePlacement/ObstaclePlacementState.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstaclePlacementState : MonoBehaviour
+{
+    #region GenerationVariables
+    private static ObstaclePlacementState instance;
+
+    private PlayerMovement playerMovementScript;
+    private PlaneMovement planeMovementScript;
+
+    [SerializeField] private PlayerMovement.State playerGenerationState;
+
+    private OceanPlacement oceanScript;
+    private SkyPlacement skyScript;
+    private CavePlacement caveScript;
+    #endregion
+
+    private void OnEnable()
+    {
+        //Instance generation
+        InstantiateSingleton();
+
+        playerMovementScript = transform.parent.GetComponentInChildren<PlayerMovement>();
+        playerGenerationState = playerMovementScript.playerState;
+
+        planeMovementScript = transform.parent.GetComponent<PlaneMovement>();
+
+        oceanScript = GetComponent<OceanPlacement>();
+        skyScript = GetComponent<SkyPlacement>();
+        caveScript = GetComponent<CavePlacement>();
+    }
+
+    private void InstantiateSingleton()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    private void Update()
+    {
+        if(playerGenerationState != playerMovementScript.playerState)
+        {
+            playerGenerationState = playerMovementScript.playerState;
+        }
+
+        GenerationStateMachine();
+    }
+
+    private void GenerationStateMachine()
+    {
+        switch (playerGenerationState)
+        {
+            case PlayerMovement.State.WHALE:
+                oceanScript.enabled = true;
+                skyScript.enabled = false;
+                caveScript.enabled = false;
+                break;
+
+            case PlayerMovement.State.PLANE:
+                oceanScript.enabled = false;
+                skyScript.enabled = true;
+                caveScript.enabled = false;
+                break;
+
+            case PlayerMovement.State.FLARE:
+                oceanScript.enabled = false;
+                skyScript.enabled = false;
+                caveScript.enabled = true;
+                break;
+        }
+    }
+}
---
+++ AfterlifeProject/Assets/Scripts/ObstaclePlacement/OceanPlacement.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OceanPlacement : MonoBehaviour
+{
+    [Header("Plane Transform")]
+    [SerializeField] private Transform planeTransform;
+
+    [Space]
+    [Header("Object Pooler")]
+    [SerializeField] private ObjectPooler pooler;
+
+    private List<GameObject> objectList;
+
+    [SerializeField] private GameObject oceanFloor;
+
+
+    [SerializeField] private int floorIncrement = 0;
+
+    private void OnEnable()
+    {
+        InstantiatePrefabs();
+
+        planeTransform = transform.parent;
+
+        objectList = new List<GameObject>();
+    }
+
+    private void Start()
+    {
+        pooler = ObjectPooler.instance;
+        if (oceanFloor != null)
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                objectList.Add(pooler.NewObject(oceanFloor, new Vector3(planeTransform.position.x, planeTransform.position.y - 8f, (i * oceanFloor.transform.localScale.z) * 10), Quaternion.identity));
+                floorIncrement++;
+            }
+        }
+    }
+
+    private void Update()
+    {
+        foreach(GameObject obj in objectList)
+        {
+            MoveFloorTile(obj);
+        }
+    }
+
+    private void InstantiatePrefabs()
+    {
+        oceanFloor = Resources.Load<GameObject>("OceanFloor");
+    }
+
+    private void MoveFloorTile(GameObject obj)
+    {
+        if (obj.transform.position.z <= planeTransform.position.z - (oceanFloor.transform.localScale.z * 5))
+        {
+            obj.transform.position = new Vector3(planeTransform.position.x, planeTransform.position.y - 8f, (floorIncrement * oceanFloor.transform.localScale.z) * 10);
+            floorIncrement++;
+        }
+    }
+
+}
